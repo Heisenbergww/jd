02ab7c114d7c09f7f6563c387dae4ce0a48fbc4d
Heisenbergww <804454576@qq.com>
2018-11-28 12:57:57 +0800

    orange add tree menu

---
+++ src/js/treemenu.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    // $('.tree li:has(ul)').addClass('parent_li').find(' > ul li').css({'display':'none'})
+    $('.tree li.parent_li > span').on('click', function (e) {
+
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+
+        if (children.is(":visible")) {
+
+            children.hide('fast');
+
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-flag').removeClass('icon-minus-sign');
+
+        } else {
+
+            children.show('fast');
+
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-flag').removeClass('icon-plus-sign');
+
+        }
+
+        e.stopPropagation();
+
+    });
+})
